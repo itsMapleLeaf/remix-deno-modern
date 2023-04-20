5ca9ee161bd8f3c3a2d1239f951c416aa446a1fa
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2023-04-19 23:36:43 -0500

    working remix deno project!

---
+++ server.ts
@@ -0,0 +1,14 @@
+import { createRequestHandlerWithStaticFiles } from "@remix-run/deno"
+import { serve } from "https://deno.land/std@0.128.0/http/server.ts"
+// Import path interpreted by the Remix compiler
+import * as build from "@remix-run/dev/server-build"
+
+const remixHandler = createRequestHandlerWithStaticFiles({
+  build,
+  mode: Deno.env.get("NODE_ENV"),
+  getLoadContext: () => ({}),
+})
+
+const port = Number(Deno.env.get("PORT")) || 8000
+console.log(`Listening on http://localhost:${port}`)
+serve(remixHandler, { port })
